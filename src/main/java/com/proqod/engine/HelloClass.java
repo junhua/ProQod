4118aee39a3a0ffe2e1413bafeb1d00f22c24e67
Liu Junhua <junhua.viva@gmail.com>
2015-01-20 23:29:17 +0800

    fix helloclass message1 change back to message

--- src/main/java/com/proqod/engine/HelloClass.java
+++ src/main/java/com/proqod/engine/HelloClass.java
@@ -1,16 +1,16 @@
 package com.proqod.engine;
 
 public class HelloClass {
-    public String message1 = "Hello World";
+    public String message = "Hello World";
 
     public HelloClass () {
     }
 
     public HelloClass (String name) {
-        this.message1 = "Hello " + name + "!";
+        this.message = "Hello " + name + "!";
     }
 
     public String getMessage() {
-        return message1;
+        return message;
     }
 }
